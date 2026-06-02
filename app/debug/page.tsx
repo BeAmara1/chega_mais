22c731bb9b3b454ffe0fe52919209334d5fe3b1a
AmaralBe <be21amaral@gmail.com>
2026-06-02 10:29:12 -0300

    add debug page to test routing

---
+++ app/debug/page.tsx
@@ -0,0 +1,4 @@
+export const dynamic = 'force-dynamic'
+export default function DebugPage() {
+  return <h1 style={{color:'white',background:'#0F0F0F',padding:40,fontSize:32}}>DEBUG_PAGE_WORKS</h1>
+}
