2b3f64b1f3342933ee30ee86028ded89b04bf897
AmaralBe <be21amaral@gmail.com>
2026-06-15 09:32:16 -0300

    fix: dynamic import of named export GraffitiOverlay via .then() wrapper

--- components/graffiti-wrapper.tsx
+++ components/graffiti-wrapper.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic'
 
-const GraffitiOverlay = dynamic(() => import('@/components/graffiti-overlay'), { ssr: false })
+const GraffitiOverlay = dynamic(() => import('@/components/graffiti-overlay').then(m => ({ default: m.GraffitiOverlay })), { ssr: false })
 
 export default function GraffitiWrapper() {
   return <GraffitiOverlay />
